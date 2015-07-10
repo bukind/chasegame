05f2218466f2410eb471ff79b78d39e9e4de6e80
Dmitry Bukin <bukind@indoaustinvestments.com>
2015-07-10 13:59:40 +1000

    Add unit tests

--- chasegamelib/FreeMove.cs
+++ chasegamelib/FreeMove.cs
@@ -15,6 +15,17 @@ namespace chasegame
         public float y;
         public WrapType wrap;
         public Sprite sprite;
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}",x0,x);
+        }
+
+        public static int SortByX(FreeMove a, FreeMove b)
+        {
+            return (a.x < b.x) ? -1 : ((a.x > b.x) ? 1 : 0);
+        }
+
     }
 }
 
---
+++ libtest/Test1.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using chasegame;
+
+namespace chasegame
+{
+    [TestFixture()]
+    public class Test1
+    {
+        private Random random;
+
+        public Test1()
+        {
+            random = new Random();
+        }
+
+
+        private float GetF(double range)
+        {
+            return (float)(random.NextDouble() * range);
+        }
+
+
+        [Test()]
+        public void TestSort()
+        {
+            var list = new List<FreeMove>();
+            const int len = 10;
+            list.Capacity = len;
+            for (int i = 0; i < len; ++i) {
+                var s = new FreeMove();
+                s.x = GetF(100.0);
+                s.y = GetF(50.0);
+                s.x0 = GetF(100.0);
+                s.y0 = GetF(50.0);
+                list.Add(s);
+            }
+
+            Console.WriteLine("The list is [{0}]", string.Join(", ", list));
+            list.Sort(FreeMove.SortByX);
+            Console.WriteLine("The list is [{0}]", string.Join(", ", list));
+
+            Assert.That(false);
+        }
+        /*
+        public static void Main(string[] args)
+        {
+            var t = new Test1();
+            t.TestSort();
+        }
+        */
+    }
+}
+
