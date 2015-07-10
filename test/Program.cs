8ed2f73c4a52d93bb77b14edd1f4745c3cfe270a
Dmitry Bukin <bukin.dima@gmail.com>
2015-07-10 23:03:57 +1000

    Add test

---
+++ test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			var t = new Test1();
+			t.Run();
+        }
+    }
+}
---
+++ test/Test1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using chasegame;
+
+namespace test
+{
+	public class Test1
+	{
+		public Test1()
+		{
+		}
+
+		public void Run()
+		{
+			Vec v = new Vec(1.0,1.0);
+			Console.WriteLine("v={0}", v);
+			v = v.Scale(5.0).Add(new Vec(3.0,-2.0));
+			Console.WriteLine("v={0}", v);
+		}
+	}
+}
+
